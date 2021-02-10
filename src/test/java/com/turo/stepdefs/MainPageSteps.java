18e370d91ba9a1fc5943d9efb8021b0e5682da3b
ZennT <zenn.diamond@gmail.com>
2021-02-10 02:04:20 -0800

    created MainPage.feature and steps classes

---
+++ src/test/java/com/turo/stepdefs/MainPageSteps.java
@@ -0,0 +1,22 @@
+package com.turo.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MainPageSteps {
+    @Given("user is in landing page")
+    public void userIsInLandingPage() {
+    }
+
+    @Then("Verify that Turo logo is present")
+    public void verifyThatTuroLogoIsPresent() {
+    }
+
+    @Then("Verify that {string} is present")
+    public void verifyThatIsPresent(String arg0) {
+    }
+
+    @Then("Verify that Liberty mutual insurance logo is present")
+    public void verifyThatLibertyMutualInsuranceLogoIsPresent() {
+    }
+}
---
+++ src/test/resources/features/MainPage.feature
@@ -0,0 +1,7 @@
+Feature: Verifying the main page
+  Scenario: Homepage verification
+    Given user is in landing page
+    Then Verify that Turo logo is present
+    Then Verify that "Log in" is present
+    And Verify that "Sign up" is present
+    Then Verify that Liberty mutual insurance logo is present
