e3c663f44700829ca8d2534e2f500be2aeba910c
ZennT <zenn.diamond@gmail.com>
2021-02-12 22:34:04 -0800

    created Search A car page and step definition page

---
+++ src/test/java/com/turo/stepdefs/SearchACarPageSteps.java
@@ -0,0 +1,80 @@
+package com.turo.stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class SearchACarPageSteps {
+    @Given("user is on landing page to search")
+    public void userIsOnLandingPageToSearch() {
+
+    }
+
+    @Then("User enter {string} in Where text box")
+    public void userEnterInWhereTextBox(String arg0) {
+    }
+
+    @Then("User select {string} and {string} under From")
+    public void userSelectAndUnderFrom(String arg0, String arg1) {
+    }
+
+    @Then("User select {string} and {string} under Until")
+    public void userSelectAndUnderUntil(String arg0, String arg1) {
+    }
+
+    @Then("User clicks Search button")
+    public void userClicksSearchButton() {
+    }
+
+    @And("User clicks on More filters menu and select All-Star Host")
+    public void userClicksOnMoreFiltersMenuAndSelectAllStarHost() {
+    }
+
+    @And("select CAR Vehicle types and select {string} then clicks on upper arrow")
+    public void selectCARVehicleTypesAndSelectThenClicksOnUpperArrow(String arg0) {
+    }
+
+    @And("User adjust the Vehicle years range")
+    public void userAdjustTheVehicleYearsRange() {
+    }
+
+    @Then("User select the Transmission clicks on view results")
+    public void userSelectTheTransmissionClicksOnViewResults() {
+    }
+
+    @And("User clicks on Sort By menu and select an option then clicks on apply")
+    public void userClicksOnSortByMenuAndSelectAnOptionThenClicksOnApply() {
+    }
+
+    @And("User clicks on Price menu and adjust the price range then clicks on view results")
+    public void userClicksOnPriceMenuAndAdjustThePriceRangeThenClicksOnViewResults() {
+    }
+
+    @And("User clicks on Book instantly menu and clicks on view results")
+    public void userClicksOnBookInstantlyMenuAndClicksOnViewResults() {
+    }
+
+    @And("User clicks on Delivery menu and adjust the distance range then clicks on view results")
+    public void userClicksOnDeliveryMenuAndAdjustTheDistanceRangeThenClicksOnViewResults() {
+    }
+
+    @And("User clicks on Distance included menu and select the distance then clicks on  apply button")
+    public void userClicksOnDistanceIncludedMenuAndSelectTheDistanceThenClicksOnApplyButton() {
+    }
+
+    @Then("User selects the first car in the list")
+    public void userSelectsTheFirstCarInTheList() {
+    }
+
+    @And("Verify that Pickup & return location is present")
+    public void verifyThatPickupReturnLocationIsPresent() {
+    }
+
+    @Then("User selects Pickup & return location")
+    public void userSelectsPickupReturnLocation() {
+    }
+
+    @And("User clicks on Continue button")
+    public void userClicksOnContinueButton() {
+    }
+}
